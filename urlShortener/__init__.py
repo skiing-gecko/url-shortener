f703a3334da20d58375383d86fc100c0cafc357f
skiing-gecko <harvey.balaam@durham.ac.uk>
2023-07-16 15:37:38 -0400

    Add app initialisation and auth blueprint

---
+++ urlShortener/__init__.py
@@ -0,0 +1,40 @@
+"Create Flask application"
+
+# Based on Flask tutorial: https://flask.palletsprojects.com/en/2.3.x/tutorial/
+
+import os
+from flask import Flask
+
+
+def create_app(test_config=None):
+    "Create Flask app"
+
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY="dev",
+        DATABASE=os.path.join(app.instance_path, "urlShortener.sqlite"),
+    )
+
+    if test_config is None:
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route("/hello")
+    def hello():
+        return "hello, world!"
+
+    from . import db
+
+    db.init_app(app)
+
+    from . import auth
+
+    app.register_blueprint(auth.bp)
+
+    return app
