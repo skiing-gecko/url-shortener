6a317db72440fe52322b43da10d13ae805a79474
skiing-gecko <harvey.balaam@durham.ac.uk>
2023-07-16 15:38:39 -0400

    User registration

---
+++ urlShortener/auth.py
@@ -0,0 +1,53 @@
+"Authentication for url-shortener users"
+
+import functools
+
+from flask import (
+    Blueprint,
+    flash,
+    g,
+    redirect,
+    render_template,
+    request,
+    session,
+    url_for,
+)
+
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from urlShortener.db import get_db
+
+bp = Blueprint("auth", __name__, url_prefix="/auth")
+
+
+@bp.route("/register", methods=("GET", "POST"))
+def register():
+    "Register a new user"
+
+    if request.method == "GET":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        db = get_db()
+        error = None
+
+        if not username:
+            error = "Username is required"
+        elif not password:
+            error = "Password is required"
+
+        if error is None:
+            try:
+                db.execute(
+                    "INSERT INTO user (username, password) VALUES (?, ?)",
+                    (username, generate_password_hash(password)),
+                )
+                db.commit()
+            except db.IntegrityError:
+                error = f"User {username} is already registered"
+            else:
+                return redirect(url_for("auth.login"))
+
+        flash(error)
+
+    return render_template("auth/register.html")
