100c275586e7ee72b59a050182e5246958ec2797
skiing-gecko <harvey.balaam@durham.ac.uk>
2023-07-22 12:29:37 -0400

    Add load_logged_in_user() to check if a user is already logged in

--- urlShortener/auth.py
+++ urlShortener/auth.py
@@ -78,3 +78,15 @@ def login():
         flash(error)
 
     return render_template("auth/login.html")
+
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get("user_id")
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            "SELECT * FROM user WHERE id = ?", ("user_id",)
+        ).fetchone()
