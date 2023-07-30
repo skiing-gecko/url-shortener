1485b007fc675bf7bbef24dd9b0805da0717ef35
skiing-gecko <harvey.balaam@durham.ac.uk>
2023-07-30 18:07:03 +0200

    Add urls blueprint to Flask app

--- urlShortener/__init__.py
+++ urlShortener/__init__.py
@@ -37,4 +37,9 @@ def create_app(test_config=None):
 
     app.register_blueprint(auth.bp)
 
+    from . import urls
+
+    app.register_blueprint(urls.bp)
+    app.add_url_rule("/", endpoint="index")
+
     return app
