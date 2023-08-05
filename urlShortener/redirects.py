71afd021c96f7e9f211617071a8ce73f1df52106
skiing-gecko <harvey.balaam@durham.ac.uk>
2023-08-05 17:30:05 +0200

    Add docstring, return 404 if URL not found

--- urlShortener/redirects.py
+++ urlShortener/redirects.py
@@ -1,10 +1,20 @@
 from flask import Blueprint, redirect
 from urlShortener.db import get_db
+from werkzeug.exceptions import abort
 
 bp = Blueprint("redirects", __name__, url_prefix="/url")
 
 
 @bp.route("/<suffix>")
 def redirect_url(suffix: str):
+    """
+    Redirect to the stored URL against the random string assigned to it, or return 404 if not found in the database
+
+    :param str suffix: URL variable referring to the randomly generated string assigned to a 'long' URL
+    """
     url = get_db().execute("SELECT originalUrl FROM urls WHERE shortener_string = ?", (suffix,)).fetchone()
-    return redirect(url[0])
+
+    if url is not None:
+        return redirect(url[0])
+    else:
+        abort(404)
