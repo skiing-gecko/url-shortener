4e5f4c150694e8fb968a8820582586c441616f35
skiing-gecko <harvey.balaam@durham.ac.uk>
2023-08-08 16:32:06 +0200

    Create update URL by ID endpoint (PATCH)

--- urlShortener/api.py
+++ urlShortener/api.py
@@ -78,3 +78,32 @@ def delete_url_by_id(url_id: int):
         )
         db.commit()
         return "", 204
+
+
+@bp.route("/urls/<int:url_id>", methods=("PATCH",))
+def update_url_by_id(url_id: int):
+    key = request.headers.get("Authorization")
+
+    user_id: str = authenticate_api(key)
+
+    if (
+        get_db()
+        .execute("SELECT 1 WHERE EXISTS( SELECT 1 FROM urls WHERE id = ? )", (url_id,))
+        .fetchone()
+    ) is None:
+        abort(404)
+
+    if user_id is not None:
+        try:
+            name: str = request.json["url_name"]
+            url: str = request.json["original_url"]
+
+            db = get_db()
+            db.execute(
+                "UPDATE urls SET url_name = ?, original_url = ? WHERE id = ? AND creator_id = ?",
+                (name, url, url_id, user_id),
+            )
+            db.commit()
+            return "", 200
+        except KeyError:
+            abort(400)
