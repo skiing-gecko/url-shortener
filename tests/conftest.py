6337a5cff551d5cd4b12710e5f5cf6a1f06f5d72
skiing-gecko <harvey.balaam@durham.ac.uk>
2023-08-10 20:19:13 +0200

    Setup testing

---
+++ tests/conftest.py
@@ -0,0 +1,41 @@
+import os
+import tempfile
+
+import pytest
+
+from urlShortener import create_app
+from urlShortener.db import get_db, init_db
+
+with open(os.path.join(os.path.dirname(__file__), "data.sql"), "rb") as file:
+    _data_sql = file.read().decode("utf8")
+
+
+@pytest.fixture
+def app():
+    db_fd, db_path = tempfile.mkstemp()
+
+    # Create app in testing mode and use a temporary DB
+    app = create_app(
+        {
+            "TESTING": True,
+            "DATABASE": db_path,
+        }
+    )
+    with app.app_context():
+        init_db()
+        get_db().executescript(_data_sql)
+
+    yield app
+
+    os.close(db_fd)
+    os.unlink(db_path)
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+
+@pytest.fixture
+def runner(app):
+    return app.test_cli_runner()
