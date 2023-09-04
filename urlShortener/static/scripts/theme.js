975e75ae7e27bdca2efcff197e72a5ba810942ef
skiing-gecko <harvey.balaam@durham.ac.uk>
2023-09-04 14:59:16 +0100

    Change light/dark mode toggle

--- urlShortener/static/scripts/theme.js
+++ urlShortener/static/scripts/theme.js
@@ -1,15 +1,17 @@
 set_initial_theme();
 
 document.addEventListener('DOMContentLoaded', () => {
-  const colorButton = document.getElementById('color-button');
-  colorButton.addEventListener('click', () => swap_theme(), false);
-  colorButton.checked = localStorage.getItem('theme') !== 'light';
-
   const logoText = document.getElementById('logo-text');
+  const colorToggle = document.getElementById('color-toggle');
+
+  colorToggle.addEventListener('click', () => swap_theme_other(colorToggle, logoText), false);
+
   if (localStorage.getItem('theme') === 'light') {
     logoText.setAttribute('fill', '#303030');
+    colorToggle.children[0].setAttribute('class', 'bi bi-moon-stars-fill');
   } else {
     logoText.setAttribute('fill', '#FFF');
+    colorToggle.children[0].setAttribute('class', 'bi bi-sun-fill');
   }
 });
 
@@ -19,17 +21,18 @@ function set_initial_theme() {
   }
 }
 
-function swap_theme() {
+function swap_theme_other(button, logo) {
   if (localStorage.getItem('theme')) {
-    const logoText = document.getElementById('logo-text');
     if (localStorage.getItem('theme') === "light") {
       localStorage.setItem('theme', 'dark');
       document.documentElement.setAttribute('data-bs-theme', 'dark');
-      logoText.setAttribute('fill', '#FFF');
+      logo.setAttribute('fill', '#FFF');
+      button.children[0].setAttribute('class', 'bi bi-sun-fill');
     } else {
       localStorage.setItem('theme', 'light');
       document.documentElement.setAttribute('data-bs-theme', 'light');
-      logoText.setAttribute('fill', '#303030');
+      logo.setAttribute('fill', '#303030');
+      button.children[0].setAttribute('class', 'bi bi-moon-stars-fill');
     }
   }
 }
