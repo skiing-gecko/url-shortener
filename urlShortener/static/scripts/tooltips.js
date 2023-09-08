dd54165dd2ab0ad7ab20747359eb1d17b47a9719
skiing-gecko <harvey.balaam@durham.ac.uk>
2023-09-08 17:00:19 +0100

    refactor: move tooltip JS to external file
    Remove inline JS for the Bootstrap tooltips, to comply with incoming Content Security Policy
    
    #3

---
+++ urlShortener/static/scripts/tooltips.js
@@ -0,0 +1,4 @@
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+const offcanvasElementList = document.querySelectorAll('.offcanvas')
+const offcanvasList = [...offcanvasElementList].map(offcanvasEl => new bootstrap.Offcanvas(offcanvasEl))
